2f38e221c161d23c2451788eca7dce9992f137ad
alex-luxonis <alex@luxonis.com>
2021-04-24 18:19:41 +0300

    UpdateFlash: add BOOT_HEADER, allowing to flash a boot override header, for example for a non-volatile change to USB boot

--- include/depthai-bootloader-shared/Bootloader.hpp
+++ include/depthai-bootloader-shared/Bootloader.hpp
@@ -39,8 +39,12 @@ namespace request {
         UpdateFlash() : cmd(UPDATE_FLASH) {}
 
         // Data
-        enum Storage : uint32_t { SBR, BOOTLOADER };
+        enum Storage : uint32_t { SBR, BOOTLOADER, BOOT_HEADER };
+        // TODO make BOOT_HEADER and BOOTLOADER storage not overlap.
+        // Then we could have separate actions to update BOOT_HEADER only.
+        enum BootHeader : uint32_t { CUSTOM_PAYLOAD, USB, /* FLASH_SPI_QUAD, */ };
         Storage storage;
+        BootHeader bootHeader;
         uint32_t totalSize;
         uint32_t numPackets;
     };
