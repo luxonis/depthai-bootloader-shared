17d35e140451e396b77651291343015e809164a0
TheMarpe <martin.peterlin7@gmail.com>
2021-06-01 19:10:06 +0200

    WIP: Bootloader improvements

--- include/depthai-bootloader-shared/Bootloader.hpp
+++ include/depthai-bootloader-shared/Bootloader.hpp
@@ -1,52 +1,75 @@
 #pragma once
 
 #include <cstdint>
+#include <map>
 
 namespace dai
 {
 namespace bootloader
 {
 
+enum class Type : std::int32_t {
+    USB, ETHERNET
+};
+
+enum class Memory : std::int32_t {
+    FLASH, EMMC
+};
+
+enum class Section : std::int32_t {
+    HEADER, BOOTLOADER, BOOTLOADER_CONFIG, APPLICATION
+};
+
+struct Structure {
+    Structure() = default;
+    std::map<Section, long> offset, size;
+protected:
+    Structure(decltype(offset) a, decltype(size) b) : offset(a), size(b) {}
+};
+
 namespace request {
 
     enum Command {
         USB_ROM_BOOT = 0,
-        BOOT_APPLICATION, 
+        BOOT_APPLICATION,
         UPDATE_FLASH,
         GET_BOOTLOADER_VERSION
     };
 
+    struct ARequest {
+        ARequest() = delete;
+        Command cmd;
+    protected:
+        ARequest(Command command) : cmd(command){}
+    };
+
     // Specific request PODs
-    struct UsbRomBoot {
+    struct UsbRomBoot : ARequest {
         // Common
-        Command cmd;
-        UsbRomBoot() : cmd(USB_ROM_BOOT) {}
+        UsbRomBoot() : ARequest(USB_ROM_BOOT) {}
     };
-    struct BootApplication {
+    struct BootApplication : ARequest {
         // Common
-        Command cmd;
-        BootApplication() : cmd(BOOT_APPLICATION) {}
-    
+        BootApplication() : ARequest(BOOT_APPLICATION) {}
+
         // Data
     };
 
-    struct UpdateFlash {
+    struct UpdateFlash : ARequest {
         // Common
-        Command cmd;
-        UpdateFlash() : cmd(UPDATE_FLASH) {}
+        UpdateFlash() : ARequest(UPDATE_FLASH) {}
 
         // Data
-        enum Storage : uint32_t { SBR, BOOTLOADER };
-        Storage storage;
+        Memory memory;
+        Section section;
         uint32_t totalSize;
         uint32_t numPackets;
     };
 
-    struct GetBootloaderVersion {
+    struct GetBootloaderVersion : ARequest {
         // Common
-        Command cmd;
-        GetBootloaderVersion() : cmd(GET_BOOTLOADER_VERSION) {}
-    
+        GetBootloaderVersion() : ARequest(GET_BOOTLOADER_VERSION) {}
+
         // Data
     };
 
@@ -56,34 +79,38 @@ namespace request {
 namespace response {
 
     enum Command : uint32_t {
-        FLASH_COMPLETE = 0, 
+        FLASH_COMPLETE = 0,
         FLASH_STATUS_UPDATE,
         BOOTLOADER_VERSION
     };
 
-    // Specific request PODs
-    struct FlashComplete{
-        // Common
+    struct AResponse {
+        AResponse() = delete;
         Command cmd;
-        FlashComplete() : cmd(FLASH_COMPLETE) {}
+    protected:
+        AResponse(Command command) : cmd(command){}
+    };
+
+    // Specific request PODs
+    struct FlashComplete : AResponse {
+        // Common
+        FlashComplete() : AResponse(FLASH_COMPLETE) {}
 
         // Data
         uint32_t success;
         char errorMsg[64];
     };
-    struct FlashStatusUpdate {
+    struct FlashStatusUpdate : AResponse {
         // Common
-        Command cmd;
-        FlashStatusUpdate() : cmd(FLASH_STATUS_UPDATE) {}
+        FlashStatusUpdate() : AResponse(FLASH_STATUS_UPDATE) {}
 
         // Data
         float progress;
     };
-    struct BootloaderVersion {
+    struct BootloaderVersion : AResponse {
         // Common
-        Command cmd;
-        BootloaderVersion() : cmd(BOOTLOADER_VERSION) {}
-        
+        BootloaderVersion() : AResponse(BOOTLOADER_VERSION) {}
+
         // Data
         uint32_t major, minor, patch;
     };
