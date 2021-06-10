ed30fb7463aa1fc61d46a768538f52266d1a3f27
TheMarpe <martin.peterlin7@gmail.com>
2021-06-10 11:14:28 +0200

    Added new commands

--- include/depthai-bootloader-shared/Bootloader.hpp
+++ include/depthai-bootloader-shared/Bootloader.hpp
@@ -33,7 +33,9 @@ namespace request {
         USB_ROM_BOOT = 0,
         BOOT_APPLICATION,
         UPDATE_FLASH,
-        GET_BOOTLOADER_VERSION
+        GET_BOOTLOADER_VERSION,
+        UPDATE_FLASH_EX,
+        UPDATE_FLASH_EX_2
     };
 
     struct ARequest {
@@ -60,17 +62,42 @@ namespace request {
         UpdateFlash() : ARequest(UPDATE_FLASH) {}
 
         // Data
-        Memory memory;
-        Section section;
+        enum Storage : uint32_t { SBR, BOOTLOADER };
+        Storage storage;
         uint32_t totalSize;
         uint32_t numPackets;
     };
+
 
     struct GetBootloaderVersion : ARequest {
         // Common
         GetBootloaderVersion() : ARequest(GET_BOOTLOADER_VERSION) {}
 
         // Data
+    };
+
+    // UpdateFlashEx - Additional options
+    struct UpdateFlashEx : ARequest {
+        // Common
+        UpdateFlashEx() : ARequest(UPDATE_FLASH_EX) {}
+
+        // Data
+        Memory memory;
+        Section section;
+        uint32_t totalSize;
+        uint32_t numPackets;
+    };
+
+    // UpdateFlashEx2 - Additional options
+    struct UpdateFlashEx2 : ARequest {
+        // Common
+        UpdateFlashEx2() : ARequest(UPDATE_FLASH_EX_2) {}
+
+        // Data
+        Memory memory;
+        uint32_t offset;
+        uint32_t totalSize;
+        uint32_t numPackets;
     };
 
 }
@@ -81,7 +108,8 @@ namespace response {
     enum Command : uint32_t {
         FLASH_COMPLETE = 0,
         FLASH_STATUS_UPDATE,
-        BOOTLOADER_VERSION
+        BOOTLOADER_VERSION,
+        BOOTLOADER_TYPE
     };
 
     struct AResponse {
@@ -113,6 +141,14 @@ namespace response {
 
         // Data
         uint32_t major, minor, patch;
+    };
+
+    struct BootloaderType : AResponse {
+        // Common
+        BootloaderType() : AResponse(BOOTLOADER_TYPE) {}
+
+        // Data
+        Type type;
     };
 
 }
